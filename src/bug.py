d8bc5e4c15fa1149c87bffaa2b841b4279d36624
dheerajyadav1714 <125873179+dheerajyadav1714@users.noreply.github.com>
2026-04-05 02:08:18 +0530

    Restore broken code in calculate_metrics function
    Revert to original code that causes ZeroDivisionError.

--- src/bug.py
+++ src/bug.py
@@ -1,8 +1,2 @@
-# Intentionally broken code for testing Autonomous Agent
 def calculate_metrics():
-    # The original code 'return 100 / 0' caused a ZeroDivisionError.
-    # To fix this, we ensure that division by zero does not occur.
-    # As the denominator is hardcoded to 0, we provide a safe return value
-    # instead of performing an invalid operation. If the denominator were
-    # variable, a conditional check would be used to handle the zero case.
-    return 0 # Returning 0 as a default value for an undefined metric
+    return 100 / 0  
