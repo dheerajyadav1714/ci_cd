3504e16ad3276e1434dea7b36b8d98afe01a528f
dheerajyadav1714 <125873179+dheerajyadav1714@users.noreply.github.com>
2026-04-05 13:38:24 +0530

    Update bug.py

--- src/bug.py
+++ src/bug.py
@@ -1,6 +1,2 @@
 def calculate_metrics():
-    numerator = 100
-    denominator = 0
-    if denominator == 0:
-        return 0  # Return 0 or another appropriate default value instead of crashing
-    return numerator / denominator
+    return 100 / 0  
